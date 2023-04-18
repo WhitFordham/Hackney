0c8a80e26eeea53bb37ac097bf437984842a90d0
WhitFordham <dwfordham01@gmail.com>
2023-04-17 23:01:37 -0400

    Sign In Works

--- app/src/main/java/edu/uga/cs/hackney/MainActivity.java
+++ app/src/main/java/edu/uga/cs/hackney/MainActivity.java
@@ -1,24 +1,12 @@
 package edu.uga.cs.hackney;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
@@ -28,20 +16,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button signUp = findViewById(R.id.button3);
+        Button signIn = findViewById(R.id.button2);
+
         auth = FirebaseAuth.getInstance();
         String email = "email@email.net";
         String password = "Shrenk";
 
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                Toast.makeText(getApplicationContext(),
-                        "Registered user: " + email,
-                        Toast.LENGTH_SHORT).show();
+        signUp.setOnClickListener(view -> {
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(),
+                            "Registered user: " + email,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Sign up failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
 
-            } else {
-                Toast.makeText(MainActivity.this, "Registration failed.",
-                        Toast.LENGTH_SHORT).show();
-            }
+        signIn.setOnClickListener(view -> {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+               if (task.isSuccessful()) {
+                   Toast.makeText(getApplicationContext(),
+                           "Signed in successfully!", Toast.LENGTH_SHORT).show();
+               } else {
+                   Toast.makeText(getApplicationContext(), "Sign in failed.",
+                           Toast.LENGTH_SHORT).show();
+               }
+            });
         });
     }
 }
