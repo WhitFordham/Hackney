811479495a5c9f507e0a2b21e2dded8f8dbd1586
emilyzacharias <73804664+emilyzacharias@users.noreply.github.com>
2023-04-23 17:27:47 -0400

    user can create new rides

--- app/src/main/java/edu/uga/cs/hackney/NewRideActivity.java
+++ app/src/main/java/edu/uga/cs/hackney/NewRideActivity.java
@@ -1,12 +1,26 @@
 package edu.uga.cs.hackney;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NewRideActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     private EditText dateView;
     private EditText timeView;
@@ -26,5 +40,32 @@ public class NewRideActivity extends AppCompatActivity {
         priceView = findViewById( R.id.editTextNumberDecimal );
         Button requestButton = findViewById(R.id.button10);
         Button offerButton = findViewById(R.id.button11);
+
+        offerButton.setOnClickListener( new ButtonClickListener()) ;
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String date = dateView.getText().toString();
+            String time = timeView.getText().toString();
+            String startLocation = startLocationView.getText().toString();
+            String endLocation = endLocationView.getText().toString();
+            Double price = Double.valueOf(priceView.getText().toString());
+            final Ride rideRequest = new Ride(date + time, startLocation, endLocation, price);
+
+            auth = FirebaseAuth.getInstance();
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+            DatabaseReference reference = database.getReference("rideRequests");
+
+            reference.push().setValue(rideRequest).addOnSuccessListener(aVoid -> {
+                Toast.makeText(getApplicationContext(), "Ride Request created",
+                        Toast.LENGTH_SHORT).show();
+            }).addOnFailureListener(error -> {
+                Toast.makeText(getApplicationContext(), "Failed to create a ride request",
+                        Toast.LENGTH_SHORT).show();
+            });
+        }
     }
 }
