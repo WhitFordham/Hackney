bc95149049cbce3dc15c9fc23348c7c5ffc5f233
emilyzacharias <73804664+emilyzacharias@users.noreply.github.com>
2023-04-26 15:39:59 -0400

    manage user rides layout

--- app/src/main/java/edu/uga/cs/hackney/PointsActivity.java
+++ app/src/main/java/edu/uga/cs/hackney/PointsActivity.java
@@ -3,6 +3,10 @@ package edu.uga.cs.hackney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PointsActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class PointsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points);
+
+        TextView pointsText = findViewById(R.id.textView9);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+
+       // pointsText.setText();
     }
 }
